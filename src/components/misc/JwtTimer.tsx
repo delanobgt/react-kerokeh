2db6f554161c59e303576ce1ec96403e87439382
delanobgt <irvindelano@gmail.com>
2020-02-05 13:28:41 +0700

    jwt timer with snackbar

--- src/components/misc/JwtTimer.tsx
+++ src/components/misc/JwtTimer.tsx
@@ -7,21 +7,63 @@ import { RootState } from "src/store";
 import BasicDialog from "src/components/generic/dialog/BasicDialog";
 import Link from "src/components/generic/Link";
 import { JWToken } from "src/util/types";
+import { useSnackbar } from "material-ui-snackbar-provider";
 
 const JwtTimer = () => {
   const token = useSelector<RootState, string>(state => state.auth.token);
   const [dialogOpen, setDialogOpen] = React.useState(false);
+  const snackbar = useSnackbar();
 
   React.useEffect(() => {
+    if (!token) return;
     const decoded: JWToken = jwtDecode(token);
-    const timeoutDelay = Math.max(0, decoded.exp * 1000 - moment().valueOf());
-    const expTimeout = setTimeout(() => {
-      setDialogOpen(true);
-    }, timeoutDelay);
+    const expiredTimeLeftMs = Math.max(
+      0,
+      decoded.exp * 1000 - moment().valueOf()
+    );
+    const timeouts: number[] = [];
+    timeouts.push(
+      setTimeout(() => {
+        setDialogOpen(true);
+      }, expiredTimeLeftMs)
+    );
+    // minutes
+    [
+      5,
+      4,
+      3,
+      2,
+      1,
+      30 / 60,
+      15 / 60,
+      5 / 60,
+      4 / 60,
+      3 / 60,
+      2 / 60,
+      1 / 60
+    ].forEach(minute => {
+      console.log("before", minute);
+      const timeoutDelayMs = expiredTimeLeftMs - minute * 60 * 1000;
+      if (timeoutDelayMs > 0) {
+        timeouts.push(
+          setTimeout(() => {
+            const time = minute < 1 ? minute * 60 : minute;
+            const unit = minute < 1 ? "seconds" : "minutes";
+            console.log("after", minute);
+            snackbar.showMessage(
+              `Your session will be expired in ${time} ${unit}`
+            );
+          }, timeoutDelayMs)
+        );
+      }
+    });
+
     return () => {
-      clearTimeout(expTimeout);
+      for (const timeout of timeouts) {
+        clearTimeout(timeout);
+      }
     };
-  }, [token]);
+  }, [token, snackbar]);
 
   return (
     <BasicDialog
